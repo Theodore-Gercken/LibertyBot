d9b6673a00d65a99a327c79ecfb122384d9ef8b2
Theodore Gercken <69394182+Theodore-Gercken@users.noreply.github.com>
2021-06-28 07:50:21 -0700

    Updated links

--- Commands/links.js
+++ Commands/links.js
@@ -10,8 +10,9 @@ module.exports = {
 	    .setDescription('Here are the important links associated with this bot.')
       .addFields(
 		    { name: 'Invitation Link', value: '[Link](https://discord.com/oauth2/authorize?client_id=853455948848300043&scope=bot+applications.commands)'},
+        { name: 'Code', value: '[Link](https://github.com/Theodore-Gercken/LibertyBot)'},
 		    { name: 'Event Creation Form', value: `[Link](https://forms.gle/DXSAaWDTWyCRTGZf7)`},
-        { name: 'Creators Website', value: `[Link](https://www.theodoregercken.com)`},
+        { name: 'Patreon', value: '*Making LibertyBot available to everyone 24/7 costs $8 a month, so if you want to help out to make sure it keeps existing, feel free to donate. Any money in excess of what is needed to host LibertyBot will be donated to the Libertarian Youth Caucus or the Frontier Project.*\n[Link](https://www.patreon.com/libertybot)'},
 	    )
       .setThumbnail('https://i.postimg.cc/3RzDY40n/Logo.png')
 	    .setTimestamp()
