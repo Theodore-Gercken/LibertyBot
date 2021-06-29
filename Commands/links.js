65add768eae7c17d6d82be84a8b84e75c1080534
Theodore Gercken <69394182+Theodore-Gercken@users.noreply.github.com>
2021-06-28 18:49:13 -0700

    Made donation language more clear

--- Commands/links.js
+++ Commands/links.js
@@ -12,7 +12,7 @@ module.exports = {
 		    { name: 'Invitation Link', value: '[Link](https://discord.com/oauth2/authorize?client_id=853455948848300043&scope=bot+applications.commands)'},
         { name: 'Code', value: '[Link](https://github.com/Theodore-Gercken/LibertyBot)'},
 		    { name: 'Event Creation Form', value: `[Link](https://forms.gle/DXSAaWDTWyCRTGZf7)`},
-        { name: 'Patreon', value: '*Making LibertyBot available to everyone 24/7 costs $8 a month, so if you want to help out to make sure it keeps existing, feel free to donate. Any money in excess of what is needed to host LibertyBot will be donated to the Libertarian Youth Caucus or the Frontier Project.*\n[Link](https://www.patreon.com/libertybot)'},
+        { name: 'Patreon', value: '*Hosting LibertyBot 24/7 costs $8 a month, so if you want to help out to make sure it keeps existing as a free resource for everyone, feel free to donate to the patreon. Any money in excess of what is needed to host LibertyBot will be donated to the Libertarian Youth Caucus or the Frontier Project.*\n[Link](https://www.patreon.com/libertybot)'},
 	    )
       .setThumbnail('https://i.postimg.cc/3RzDY40n/Logo.png')
 	    .setTimestamp()
