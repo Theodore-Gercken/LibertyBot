1e43bb2660b907d5e4f2e8c9c23844cbd02887b4
Theodore Gercken <69394182+Theodore-Gercken@users.noreply.github.com>
2021-07-14 11:30:52 -0700

    Updated info

--- Commands/info.js
+++ Commands/info.js
@@ -12,7 +12,7 @@ module.exports = {
 	    .addFields(
 		    { name: 'Liberty Commands', value: '-`events`: See a list of libertarian events taking place across the internet.\n-`lp`: Look up official LP links.\n-`caucuses`: Look up official LP caucus links.\n-`discords`: Join other libertarian discord servers.\n-`news`: Find libertarian news sources covering the party and politics in general.\n-`nolan`: Take the Nolan Political Test to see where you fall on the political compass.\n-`libertarianism`: Engage in a short conversation with the bot to explore the foundations of libertarian philosophy.\n-`taxation`: Learn why taxation is theft.'},
 		    { name: 'Bot Commands', value: '-`info`: Pulls up general information about the bot.\n-`links`: Pulls up all of the bots relevant links.\n-`ping`: Checks to see if the bot is still connected. It should respond with pong!'},
-        { name: 'Invite LibertyBot To Another Server!', value: `[Invite Link](https://discord.com/oauth2/authorize?client_id=853455948848300043&scope=bot+applications.commands)`},
+        { name: 'Invite LibertyBot To Another Server!', value: `Invite the bot to another server [here](https://discord.com/oauth2/authorize?client_id=853455948848300043&scope=bot+applications.commands)\nCheck out the website [here](https://top.gg/bot/853455948848300043)`},
 	    )
 	    .setTimestamp()
 	    .setFooter('I was created by Theodore Gercken, email contact.theodoregercken@gmail.com to request a feature, report a bug/typo, or just get in touch!');
